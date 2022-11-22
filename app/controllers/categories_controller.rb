ce1593fee4423e7d83aba446067fbfb2cd52f8e5
ijeomaodinko <ijeomaodinko@gmail.com>
2022-11-22 18:26:44 -0500

    Add setup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,33 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
+  load_and_authorize_resource
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category =Category.new 
   end
 
   def create
+    @category = Category.create(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_url, notice: 'Category was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+  end
+
+  private 
+  
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,13 +1,34 @@
 class ExpensesController < ApplicationController
+  before_action :set_expense, only: %i[show edit update destroy]
+  before_action :set_category, only: %i[index show new edit update create destroy]
+  load_and_authorize_resource
+
   def index
+    
   end
 
   def new
+    @expense << category
   end
 
   def create
+    @expense = Expense.create(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_url, notice: 'Category was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+  end
+
+  private 
+
+  def category_params
+    params.require(:expense).permit(:name, :icon, category.id[])
   end
 end
