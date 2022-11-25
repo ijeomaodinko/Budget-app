d8b10dd0a5e73cb89ef053e3a95e22008730b079
ijeomaodinko <ijeomaodinko@gmail.com>
2022-11-25 16:16:08 -0500

    Add file test

---
+++ spec/request/expense_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ExpensesController, type: :request do
+  describe "GET './index' page" do
+    before(:all) do
+      @route = get '/expenses/index'
+    end
+
+    it 'it should return a valid status' do
+      expect(@route).to eq(302)
+    end
+  end
+
+   describe 'GET /new' do
+    it 'returns http success' do
+      get '/expenses/new'
+      expect(response).to have_http_status(302)
+    end
+  end
+end
