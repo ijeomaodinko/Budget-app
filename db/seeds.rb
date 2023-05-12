4d9609c039923e7842fda585fab31939d53cc175
ijeomaodinko <ijeomaodinko@gmail.com>
2023-05-12 12:42:38 -0400

    Add files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+categories = Categories.create([
+    {
+        name: "Funiture",
+        icon: ""
+    },
+    {
+        name: "Food",
+        icon: ""
+    }
+    {
+        name: "Transportation"
+        icon: ""
+    }
+])
+
+expenses = Expenses.create ([
+    {
+        name: "Table",
+        amount: 1500
+    }
+    {   
+        name: "Magezines"
+        amount: 300
+    }
+]),
+
+Users.create(name: "example",  gmail: "example@example.com", password: "example" )
