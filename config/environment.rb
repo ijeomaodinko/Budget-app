b5fa709f87319420b0b64ab6db3d6ecafd1e31a1
Ijeoma Odinko <95123554+ijeomaodinko@users.noreply.github.com>
2023-05-24 04:43:57 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.application.secrets.secret_key_base = '5dc942078e45043d8129fc5de2ab4be7'
+
 # Initialize the Rails application.
 Rails.application.initialize!
