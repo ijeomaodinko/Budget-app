b680995c99bfb2ed0dd6c07143b8ab237b74a598
ijeomaodinko <ijeomaodinko@gmail.com>
2023-05-24 08:47:33 -0400

    Add files

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-Rails.application.secrets.secret_key_base = '1105090a55dd3d2a8c3967ff228cbe05'
-
 # Initialize the Rails application.
 Rails.application.initialize!
