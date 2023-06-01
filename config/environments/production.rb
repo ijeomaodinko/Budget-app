d9bf1dce224a84946b6a9c394bbd46e46c331ff4
ijeomaodinko <ijeomaodinko@gmail.com>
2023-06-01 16:57:40 -0400

    Add files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'https://budget-app-962l.onrender.com',
+      :user_name => 'budgetapp',
+      :password => 'budgetapp',
+      :authentication => :plain,
+      :enable_starttls_auto => true 
+  } 
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
