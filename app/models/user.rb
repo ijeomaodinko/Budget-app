5b9598af71d9befd3d6936285709ddaeb1ed2edc
ijeomaodinko <ijeomaodinko@gmail.com>
2023-06-06 12:17:05 -0400

    Add file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :registerable,
   devise :database_authenticatable, :registerable,
-         :confirmable, :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
   has_many :categories
   has_many :expenses
